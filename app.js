a06c0e859c8c9ad77a9855e7e950133e76ec48dd
Alex <alex.yatseiko@gmail.com>
2018-06-20 15:47:55 +0300

    post model and post routes added

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const postRouter = require('./routes/post')
+const keys = require('./keys')
 
-const port = 5000
+const port = process.env.PORT || 5000
+const clientPath = path.join(__dirname, 'client')
+
+mongoose.connect(keys.mongoURI)
+	.then(() => console.log('MongoDB connected')) // In case connetion is OK
+	.catch(err => console.error(err)) // In case something is wrond
 const app = express()
+app.use('/api/', postRouter)
+app.use(express.static(clientPath))
+
 app.listen(port, () => {
-	console.log(`Server has been started on port ${prot}`)
+	console.log(`Server has been started on port ${port}`)
 })
 
---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+	mongoURI: 'mongodb://forest:sHIPPUUDEN987@ds161740.mlab.com:61740/fullstack-js-blog'
+}
